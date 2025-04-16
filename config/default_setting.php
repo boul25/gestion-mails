b57f30fa342fbd90b4418777197436aca4270d82
boul25 <boul25@gmail.com>
2025-04-16 09:16:56 +0300

    rajout default_setting

---
+++ config/default_setting.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fichier       : setting.php
+ * Sujet         : Sujet du fichier
+ * Auteur        : Mamitiana Ramanandraitsiory <boul25@gmail.com>
+ * Créé le       : 2025-04-16
+ * Dernière mod. : 2025-04-16
+ *
+ * Description   : script de configuration de la base de données
+ */
+
+ return[
+    "host" => "localhost",
+    "dbname" => "gestion-contacts",
+    "username"=> "mamu",
+    "password"=> "mysqlmamy1976@"
+ ];
